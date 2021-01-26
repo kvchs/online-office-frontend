82085c3e9d68ab7da08a7b88c2322238a268e86e
chenyingchao <chenyingchao@xigua.city>
2021-01-27 00:21:50 +0800

    完善登录页校验

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,20 +1,20 @@
 <template>
     <div>
-        <el-form ref="form" :model="loginForm" class="loginContainer" >
+        <el-form ref="loginForm" :model="loginForm" class="loginContainer" :rules="rules">
             <h3 class="loginTitle">登录系统</h3>
-            <el-form-item >
+            <el-form-item prop="username">
                 <el-input type="text" v-model="loginForm.username" placeholder="请输入用户名" auto-complete="false"></el-input>
             </el-form-item>
-            <el-form-item >
-                <el-input v-model="loginForm.passwork" placeholder="请输入密码" type="password" auto-complete="false"></el-input>
+            <el-form-item prop="password" >
+                <el-input v-model="loginForm.password" placeholder="请输入密码" type="password" auto-complete="false"></el-input>
             </el-form-item>
-            <el-form-item >
+            <el-form-item prop="code">
                 <el-input v-model="loginForm.code" placeholder="点击图片更换验证码" type="text" auto-complete="false" style="width:200px; margin-right:5px"></el-input>
                 <img :src="captchaUrl">
                
             </el-form-item>
             <el-checkbox label="" v-model="checked" class="remember">记住密码</el-checkbox>
-            <el-button type="primary" style="width:100%">登录</el-button>
+            <el-button type="primary" style="width:100%" @click="loginSubmit">登录</el-button>
             
         </el-form>
     </div>
@@ -32,7 +32,29 @@ export default {
                 code: ''
 
             },
-            checked:true
+            checked:true,
+            rules:{
+                username: [{required: true, message:'请输入用户名', trigger: 'blur'}],
+                password: [{required: true, message:'请输入密码', trigger: 'blur'}],
+                code: [{required: true, message:'请输入验证码', trigger: 'blur'}]
+            }
+        }
+    },
+    methods:{
+        loginSubmit() {
+            // alert("test message")
+
+            this.$refs.loginForm.validate(
+                (valid) => {
+                    if(valid){
+                        alert("submit!!!");
+                    }else{
+                        // console.log("error submit!!!");
+                        this.$message.error("请输入必填字段")
+                        return false;
+                    }
+                }
+            );
         }
     }
 }
